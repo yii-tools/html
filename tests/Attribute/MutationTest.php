61e2451698bb4291100422314dafd73fa62a0597
terabytesoftw <terabytesoftw@gmail.com>
2023-01-21 13:50:52 +0000

    Fix tests `Mutation::class` in packages html. (#31)

--- tests/Attribute/MutationTest.php
+++ tests/Attribute/MutationTest.php
@@ -12,55 +12,55 @@ final class MutationTest extends TestCase
     public function testAutofocus(): void
     {
         $widget = $this->widget()->autofocus();
-        $this->assertSame(['autofocus' => true], $widget->attributes);
+        $this->assertSame(['autofocus' => true], $widget->getattributes());
     }
 
     public function testClass(): void
     {
         $widget = $this->widget()->class('test-class');
-        $this->assertSame(['class' => 'test-class'], $widget->attributes);
+        $this->assertSame(['class' => 'test-class'], $widget->getattributes());
     }
 
     public function testDisabled(): void
     {
         $widget = $this->widget()->disabled();
-        $this->assertSame(['disabled' => true], $widget->attributes);
+        $this->assertSame(['disabled' => true], $widget->getattributes());
     }
 
     public function testFormnovalidate(): void
     {
         $widget = $this->widget()->formnovalidate();
-        $this->assertSame(['formnovalidate' => true], $widget->attributes);
+        $this->assertSame(['formnovalidate' => true], $widget->getattributes());
     }
 
     public function testMultiple(): void
     {
         $widget = $this->widget()->multiple();
-        $this->assertSame(['multiple' => true], $widget->attributes);
+        $this->assertSame(['multiple' => true], $widget->getattributes());
     }
 
     public function testReadOnly(): void
     {
         $widget = $this->widget()->readonly();
-        $this->assertSame(['readonly' => true], $widget->attributes);
+        $this->assertSame(['readonly' => true], $widget->getattributes());
     }
 
     public function testReadOnlyWithFalse(): void
     {
         $widget = $this->widget()->readonly(false);
-        $this->assertSame(['readonly' => false], $widget->attributes);
+        $this->assertSame(['readonly' => false], $widget->getattributes());
     }
 
     public function testReadOnlyWithTrue(): void
     {
         $widget = $this->widget()->readonly(true);
-        $this->assertSame(['readonly' => true], $widget->attributes);
+        $this->assertSame(['readonly' => true], $widget->getattributes());
     }
 
     public function testRequired(): void
     {
         $widget = $this->widget()->required();
-        $this->assertSame(['required' => true], $widget->attributes);
+        $this->assertSame(['required' => true], $widget->getattributes());
     }
 
     private function widget(): object
@@ -74,7 +74,12 @@ final class MutationTest extends TestCase
             use Attribute\Readonlys;
             use Attribute\Required;
 
-            public array $attributes = [];
+            protected array $attributes = [];
+
+            public function getAttributes(): array
+            {
+                return $this->attributes;
+            }
         };
     }
 }
